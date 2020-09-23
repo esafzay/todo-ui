2e6c99f98025ce7ee9202d96a3265d7376e080c3
Muhammad Abbas <abbas@bitoasis.net>
2020-09-24 00:54:04 +0400

    replace button with div for todo task li

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,8 @@
 <template>
   <li class="d-flex align-items-center list-group-item">
-    <button :class="{completed}" @click="$emit('on-toggle')" class="btn border-0 text-left flex-grow-1">
+    <div :class="{completed}" @click="$emit('on-toggle')" class="btn text-left flex-grow-1">
       {{task}}
-    </button>
+    </div>
     <button @click="$emit('on-delete')" class="btn btn-outline-danger">
       <i class="fa fa-trash-alt"></i>
     </button>
